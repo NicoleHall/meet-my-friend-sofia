f0f1868266b8cc8439196849e37ec6ae839df28c
Nicole Hall <nhall@pairin.com>
2017-08-03 17:25:52 -0600

    removed comments from dater_profile_test.rb

--- test/models/dater_profile_test.rb
+++ test/models/dater_profile_test.rb
@@ -18,19 +18,3 @@ describe DaterProfile do
     assert_equal 1, matchmaker.dater_profiles.size
   end
 end
-
-
-
-# require "test_helper"
-#
-# # To be handled correctly this spec must end with "Integration Test"
-# describe "MatchmakerCanLogout Integration Test" do
-#   it "logged in matchmaker can log out" do
-#     login_matchmaker
-#     assert page.assert_text('Welcome, nicole')
-#
-#     click_link("Logout")
-#     refute page.has_content?('Welcome, nicole')
-#     assert_equal("/login", current_path)
-#   end
-# end
