58e5a21b5a8b5e5d777306134f51b331ba9cd173
Nicole Hall <nhall@pairin.com>
2017-08-03 18:05:09 -0600

    cleaned up logic in dater_profiles_test

--- test/models/dater_profile_test.rb
+++ test/models/dater_profile_test.rb
@@ -1,20 +1,25 @@
 require "test_helper"
 
 describe DaterProfile do
-  let(:dater_profile) { DaterProfile.new }
+
+  let(:matchmaker) { Matchmaker.create(username: "Patty Stanger", password: "password") }
+  let(:dater_profile) { matchmaker.dater_profiles.create(name: "Zosia Mamet",
+                                                         age: 24,
+                                                         zipcode: 10001,
+                                                         gender: 0,
+                                                         kids: 0,
+                                                         interested_in_men: true,
+                                                         interested_in_women: false,
+                                                         interested_in_any: false)}
+
 
   it "belongs to a matchmaker" do
-    matchmaker = Matchmaker.create(username: "Patty Stanger", password: "password")
-    matchmaker.dater_profiles.create(name: "Zosia Mamet",
-                                    age: 24,
-                                    zipcode: 10001,
-                                    gender: 0,
-                                    kids: 0,
-                                    interested_in_men: true,
-                                    interested_in_women: false,
-                                    interested_in_any: false
-                                    )
-
+    matchmaker
+    dater_profile
     assert_equal 1, matchmaker.dater_profiles.size
+  end
+
+  it "uses the enum properly" do
+    assert_equal "female", dater_profile.gender
   end
 end
